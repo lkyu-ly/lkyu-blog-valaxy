9da80140c7e81d3178b78b505065c89c9c59cfa4
lkyu <1423782024@qq.com>
2024-08-13 03:47:51 +0800

    try to solve 404 at refresh by add vercel.json deleted in commit#78de8bf back

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,6 @@
 import { defineConfig } from 'vite';
 
-// import RemoteAssets from 'vite-plugin-remote-assets'
-
 export default defineConfig({
-	// todo wait upyun assets
-	// plugins: [RemoteAssets()],
 	optimizeDeps: {
 		include: ['@waline/client/component'],
 	},
