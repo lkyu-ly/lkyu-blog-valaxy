03a0b733cc3dbb8d26c416c08cfd2e3d9a3241d5
lkyu <1423782024@qq.com>
2024-08-13 03:07:26 +0800

    update version & correct gtag

--- setup/gtag.ts
+++ setup/gtag.ts
@@ -4,7 +4,7 @@ import VueGtag, { trackRouter } from 'vue-gtag-next';
 export const install: UserModule = ({ isClient, app, router }) => {
 	if (isClient) {
 		app.use(VueGtag, {
-			property: { id: 'G-1LL0D86CY9' },
+			property: { id: 'G-V759BJWZQH' },
 		});
 
 		trackRouter(router);
